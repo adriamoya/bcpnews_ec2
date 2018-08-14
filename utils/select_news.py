d06d723e362210ee8c98daf4a5a66d4568810839
adriamoya <adriamoyaortiz@gmail.com>
2018-08-14 12:29:11 +0200

    models

--- utils/select_news.py
+++ utils/select_news.py
@@ -108,7 +108,9 @@ def models_fit_predict(crawl_date, df_subm):
                 # with BytesIO() as data:
                 # s3.Bucket(S3_BUCKET).download_fileobj(path_model, data)
                 # data.seek(0)    # move back to the beginning after writing
+                print('--> Downloading h5 file...')
                 s3.Bucket(S3_BUCKET).download_file(path_model, 'tmp_model.h5')
+                print('--> Loading the model...')
                 model = load_model('tmp_model.h5')
                 os.remove('tmp_model.h5')
 
